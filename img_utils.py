8b3e0673a48a40d0f0cd5115d187cf8d9bd4c815
pritam <pritam.karmokar@gmail.com>
2020-07-16 23:07:26 -0500

    Stub out some initial functions required for optical flow computation

---
+++ img_utils.py
@@ -0,0 +1,41 @@
+from PIL import Image
+import cv2 as cv
+import os
+
+DATASET_PATH = "./datasets/RubberWhale/"
+
+def get_image_shape(img):
+    """ returns the shape of the image """
+    return img.shape
+
+def get_image_size(img):
+    """ returns the size of the image """
+    return img.size
+
+def get_image_paths(path=DATASET_PATH, image_type='png'):
+    """ returns list of image filenames of type image_type in the given location path """
+    return [os.path.join(path, item) for item in os.listdir(path) if item.split('.')[-1] == image_type]
+
+def convert_to_grayscale(img):
+    """ returns grayscale of the input image """
+    return cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+def get_images(path=DATASET_PATH, image_type='png'):
+    """ returns list of PIL Image objects using files in the given location path """
+    
+    # save the current directory and change directory to path
+    current_directory = os.getcwd()
+    os.chdir(path)
+
+    # list items in directory
+    folder_items = os.listdir()
+
+    # build the list of Image objects
+    img_list = []
+    if len(folder_items) > 0:
+        img_list = [Image.open(item) for item in folder_items if item.split('.')[-1] == image_type]
+    
+    # change back to the previously saved current directory
+    os.chdir(current_directory)
+
+    return img_list
--- optical_flow.py
+++ optical_flow.py
@@ -3,3 +3,43 @@ from scipy import signal
 import numpy as np
 from utils import *
 
+
+# get list of the image file paths
+img_paths = get_image_paths()
+
+# open first image (handle)
+curr_frame = Image.open(img_paths[0])
+
+# open second image (handle)
+next_frame = Image.open(img_paths[1])
+
+
+'''
+	let the filter in x-direction be Gx = 0.25*[[-1,1],[-1,1]]
+	let the filter in y-direction be Gy = 0.25*[[-1,-1],[1,1]]
+	let the filter in xy-direction be Gt = 0.25*[[1,1],[1, 1]]
+	**1/4 = 0.25** for a 2x2 filter
+	'''
+
+
+def compute_optical_flow(img_1, img_2):
+    """ returns computed optical flow between two given frames I_{t-1} and I_{t}"""
+    pass
+
+def get_gradients(img_1, img_2):
+    """ returns gradients of image I(x,y,t) along x, y, t as tuple """
+    pass 
+
+def compute_horizontal_gradient(img_1, img_2):
+    """ returns gradient of the image (I_x) along the horizontal direction """
+    pass
+
+def compute_vertical_gradient(img_1, img_2):
+    """ returns gradient of the image (I_y) along the vertical direction """
+    pass
+
+def compute_temporal_gradient(img_1, img_2):
+    """ returns temporal gradient between two frames """
+    pass
+
+
