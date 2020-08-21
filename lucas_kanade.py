8344fad75ea6085f666976f01f548a07b40b7c37
Pritam Karmokar <pritam.karmokar@mavs.uta.edu>
2020-08-21 03:01:35 -0500

    Add Lucas Kanade library implementation to module

--- lucas_kanade.py
+++ lucas_kanade.py
@@ -41,8 +41,48 @@ if __name__ == "__main__":
     img_1 = preprocess_image(cv.imread(img_paths[0]))
     img_2 = preprocess_image(cv.imread(img_paths[1]))
 
-    # get images 
+    # set params for ShiTomasi corner detection
+    feature_params = dict( maxCorners = 100,
+                           qualityLevel = 0.3,
+                           minDistance = 7,
+                           blockSize = 7 )
 
+    # set parameters for lucas kanade optical flow
+    lk_params = dict( winSize  = (15,15),
+                      maxLevel = 2,
+                      criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03) )
 
+    # Create some random colors
+    color = np.random.randint(0,255,(100,3))
 
+    # Take first frame and find corners in it
+    img_gray_1 = convert_to_grayscale(img_1)
+    p1 = cv.goodFeaturesToTrack(img_gray_1, mask = None, **feature_params)
+
+    # Create a mask image for drawing purposes
+    mask = np.zeros_like(img_1)
+
+    img_gray_2 = convert_to_grayscale(img_2)
+
+    # calculate optical flow
+    p2, st, err = cv.calcOpticalFlowPyrLK( prevImg=img_gray_1, 
+                                           nextImg=img_gray_2, 
+                                           prevPts=p0, 
+                                           nextPts=None,
+                                           **lk_params )
+
+    # select good points
+    good_1 = p1[st==1]
+    good_2 = p2[st==1]
+
+    # draw tracks
+    for i, (new, old) in enumerate(zip(good_2, good_1)):
+        a,b = new.ravel()
+        c,d = old.ravel()
+        mask = cv.line(mask, (a,b), (c,d), color[i].tolist(), 2) 
+        frame = cv.circle(img_2, (a,b), 5, color[i].tolist(), -1)
+    img = cv.add(frame, mask)
+
+    cv.imshow('frame', img)
+    k = cv.waitKey(0)
 
