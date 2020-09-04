89994f549d5d8b2b8a6a5fbb033307323e06170f
Pritam Karmokar <pritam.karmokar@mavs.uta.edu>
2020-09-04 02:09:11 -0500

    Add note on screen saving

--- vca/sim/main.py
+++ vca/sim/main.py
@@ -5,6 +5,10 @@ from pygame.locals import *
 from game import Game
 
 if __name__ == "__main__":
+    # note :
+    # while the game runs press key 's' to toggle screenshot mechanism on/off
+    # initially screen saving is set to False
+
     # instantiate game 
     car_sim_game = Game()
 
@@ -13,3 +17,4 @@ if __name__ == "__main__":
 
     # run
     car_sim_game.run()
+
