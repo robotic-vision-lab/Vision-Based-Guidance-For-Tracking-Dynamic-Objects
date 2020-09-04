089fd7732d8411dd7ee901788852c1726b314b1e
Pritam Karmokar <pritam.karmokar@mavs.uta.edu>
2020-09-03 21:53:27 -0500

    Add settings module to hold game settings

---
+++ vca/sim/settings.py
@@ -0,0 +1,25 @@
+import pygame
+
+# define some colors to be used with pygame 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+DARK_GRAY = (31, 31, 31)
+LIGHT_GRAY = (128, 128, 128)
+RED = (255, 0, 0)
+GREEN = (0, 255, 255)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+
+#   S E T T I N G S  #
+
+# screen settings
+WIDTH, HEIGHT = 512, 512
+TITLE = "Car Simulation"
+BG_COLOR = DARK_GRAY
+
+# frame setting
+FPS = 30
+
+# car setting
+CAR_IMG = 'car.png'
