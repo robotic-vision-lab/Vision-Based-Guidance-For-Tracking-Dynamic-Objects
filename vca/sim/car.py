9cb61e88e2c87d8c4395a89f61c1de0e395c8886
Pritam Karmokar <pritam.karmokar@mavs.uta.edu>
2020-09-04 00:39:47 -0500

    Update car. Fix group assignment for sprite. Set initializations.

--- vca/sim/car.py
+++ vca/sim/car.py
@@ -10,21 +10,27 @@ from settings import CAR_IMG
 class Car(pygame.sprite.Sprite):
     """Defines a car sprite.
     """
-    def __init__(self, game, x, y):
-        pygame.sprite.Sprite.__init__(self) # call Sprite initializer
+    def __init__(self, game, x, y, vx=0, vy=0, ax=0, ay=0):
+        # assign itself to the all_sprites group 
+        self.groups = game.all_sprites
+
+        # call Sprite initializer with group info
+        pygame.sprite.Sprite.__init__(self, self.groups) 
         
         # assign Sprite.image and Sprite.rect attributes for this Sprite
-        self.image = game.car_img
-        self.rect = self.image.rect()
+        self.image, self.rect = game.car_img
 
+        # set kinematics
         self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2()
-        self.acceleration = pygame.Vector2()
+        self.velocity = pygame.Vector2(vx, vy)
+        self.acceleration = pygame.Vector2(ax, ay)
 
-        # set initial rect position
+        # set initial rect location to position
         self.rect.center = self.position
 
+        # hold onto the game reference
         self.game = game
+
 
     def update_kinematics(self):
         """helper function to update kinematics of object
