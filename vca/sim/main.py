6ee58ca100ffa453b672f685416ee32ead51c325
Pritam Karmokar <pritam.karmokar@mavs.uta.edu>
2020-09-04 04:31:46 -0500

    Fix import bug

--- vca/sim/main.py
+++ vca/sim/main.py
@@ -1,9 +1,22 @@
+import os
+import shutil
 import pygame 
 
 from pygame.locals import *
+from settings import *
 
 from game import Game
 
+
+import os
+import sys
+
+# add vca\ to sys.path
+cur_path = os.path.abspath(os.path.join('..'))
+if cur_path not in sys.path:
+    sys.path.append(cur_path)
+
+from utils.vid_utils import create_video_from_images
 if __name__ == "__main__":
     # note :
     # while the game runs press key 's' to toggle screenshot mechanism on/off
@@ -17,4 +30,11 @@ if __name__ == "__main__":
 
     # run
     car_sim_game.run()
+
+    if os.path.isdir(TEMP_FOLDER):
+        print("Creating video.")
+        create_video_from_images(TEMP_FOLDER, 'jpg', 'vid_out_car.avi', FPS)
+
+        # delete temp folder
+        shutil.rmtree(TEMP_FOLDER)
 
