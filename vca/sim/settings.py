d2b424b84685dd5bd10a9e3e8ff4676916d622c8
Pritam Karmokar <pritam.karmokar@mavs.uta.edu>
2020-09-17 01:02:31 -0500

    Add corrected color definitions for use in opencv

--- vca/sim/settings.py
+++ vca/sim/settings.py
@@ -7,9 +7,16 @@ WHITE = (255, 255, 255)
 DARK_GRAY = (31, 31, 31)
 LIGHT_GRAY = (128, 128, 128)
 RED = (255, 0, 0)
-GREEN = (0, 255, 255)
+GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+
+# define colors to be used with opencv (BGR)
+RED_CV = (102, 102, 255)
+GREEN_CV = (102, 255, 102)
+BLUE_CV = (255, 102, 102)
+YELLOW_CV = (0, 255, 255)
+
 
 
 #   S E T T I N G S  #
