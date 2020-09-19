bf287e73481aee6c283fb73d33a7e1b5550bbbea
Pritam Karmokar <pritam.karmokar@mavs.uta.edu>
2020-09-18 19:04:18 -0500

    Add drone and block settings

--- vca/sim/settings.py
+++ vca/sim/settings.py
@@ -8,7 +8,7 @@ DARK_GRAY = (31, 31, 31)
 LIGHT_GRAY = (128, 128, 128)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+BLUE = (102, 102, 255)
 YELLOW = (255, 255, 0)
 
 # define colors to be used with opencv (BGR)
@@ -21,21 +21,30 @@ YELLOW_CV = (0, 255, 255)
 
 #   S E T T I N G S  #
 
-# screen settings
-SCREEN_SIZE = WIDTH, HEIGHT = 512, 512
-SCREEN_DISPLAY_TITLE = "Car Simulation"
-SCREEN_BG_COLOR = DARK_GRAY
-
-# frame settings
-FPS = 30
-
 # resources settings
 ASSET_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'assets')
 TEMP_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tmp')
 
+# frame settings
+FPS = 30
+
+# screen settings
+SCREEN_SIZE = WIDTH, HEIGHT = 800, 600
+SCREEN_DISPLAY_TITLE = "Car Simulation"
+SCREEN_BG_COLOR = DARK_GRAY
+
 # car settings
 CAR_IMG = 'car.png'
-CAR_INITIAL_POSITION = (0, WIDTH//2)
-CAR_INITIAL_VELOCITY = (30, 0)
+CAR_SCALE = 0.5
+CAR_INITIAL_POSITION = (0, HEIGHT//2)
+CAR_INITIAL_VELOCITY = (90, 0)
 CAR_ACCELERATION = (0, 0)
+
+# block settings
+BLOCK_COLOR = YELLOW
+BLOCK_SIZE = BLOCK_WIDTH, BLOCK_HEIGHT = 8, 12
+
+# drone camera settings
+DRONE_VELOCITY_LIMIT = 500      # +/-
+DRONE_ACCELERATION_LIMIT = 20   # +/-
 
