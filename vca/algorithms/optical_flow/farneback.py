744afee6b71a449b5b0b6f01e227958c483803b8
Pritam Karmokar <pritam.karmokar@mavs.uta.edu>
2020-09-22 01:32:33 -0500

    Code clean up

--- vca/algorithms/optical_flow/farneback.py
+++ vca/algorithms/optical_flow/farneback.py
@@ -13,37 +13,6 @@ if vca_path not in sys.path:
 from utils import *
 
 
-""" 
-calcOpticalFlowFarneback(prev, next, flow, pyr_scale, levels, winsize, iterations, poly_n, poly_sigma, flags) -> flow
-.   @brief Computes a dense optical flow using the Gunnar Farneback's algorithm.
-.
-.   @param prev first 8-bit single-channel input image.
-.   @param next second input image of the same size and the same type as prev.
-.   @param flow computed flow image that has the same size as prev and type CV_32FC2.
-.   @param pyr_scale parameter, specifying the image scale (\<1) to build pyramids for each image;
-.   pyr_scale=0.5 means a classical pyramid, where each next layer is twice smaller than the previous
-.   one.
-.   @param levels number of pyramid layers including the initial image; levels=1 means that no extra
-.   layers are created and only the original images are used.
-.   @param winsize averaging window size; larger values increase the algorithm robustness to image
-.   noise and give more chances for fast motion detection, but yield more blurred motion field.
-.   @param iterations number of iterations the algorithm does at each pyramid level.
-.   @param poly_n size of the pixel neighborhood used to find polynomial expansion in each pixel;
-.   larger values mean that the image will be approximated with smoother surfaces, yielding more
-.   robust algorithm and more blurred motion field, typically poly_n =5 or 7.
-.   @param poly_sigma standard deviation of the Gaussian that is used to smooth derivatives used as a
-.   basis for the polynomial expansion; for poly_n=5, you can set poly_sigma=1.1, for poly_n=7, a
-.   good value would be poly_sigma=1.5.
-.   @param flags operation flags that can be a combination of the following:
-.    -   **OPTFLOW_USE_INITIAL_FLOW** uses the input flow as an initial flow approximation.
-.    -   **OPTFLOW_FARNEBACK_GAUSSIAN** uses the Gaussian \f$\texttt{winsize}\times\texttt{winsize}\f$
-.        filter instead of a box filter of the same size for optical flow estimation; usually, this
-.        option gives z more accurate flow than with a box filter, at the cost of lower speed;
-.        normally, winsize for a Gaussian window should be set to a larger value to achieve the same
-.        level of robustness.
-"""
-
-
 
 def compute_optical_flow_farneback(img_1, 
                                    img_2, 
