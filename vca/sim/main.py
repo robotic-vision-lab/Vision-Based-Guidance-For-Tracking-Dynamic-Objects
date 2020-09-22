9a6701dc7ae34084514d5709c3a51dbd95968aa9
Pritam Karmokar <pritam.karmokar@mavs.uta.edu>
2020-09-22 01:43:56 -0500

    Minor changes.

--- vca/sim/main.py
+++ vca/sim/main.py
@@ -21,7 +21,9 @@ if vca_path not in sys.path:
 from game import Game
 from game_utils import _prep_temp_folder
 from utils.vid_utils import create_video_from_images
-from utils.optical_flow_utils import get_OF_color_encoded, draw_sparse_optical_flow_arrows
+from utils.optical_flow_utils import (get_OF_color_encoded, 
+                                      draw_sparse_optical_flow_arrows,
+                                      draw_tracks)
 from utils.img_utils import convert_to_grayscale
 from algorithms.optical_flow import (compute_optical_flow_farneback, 
                                      compute_optical_flow_HS, 
@@ -30,7 +32,7 @@ from algorithms.optical_flow import (compute_optical_flow_farneback,
 
 def run_simulation():
     """Runs the game simulation. 
-    Let's us record the frames into the temp folder set in settings.
+        Lets us record the screen frames into the temp folder set in settings.
     """
     # instantiate game 
     car_sim_game = Game()
@@ -43,14 +45,14 @@ def run_simulation():
 
 
 def make_video(video_name, folder_path):
-    """Looks for frames in temp folder (set in settings module), 
-    writes them into a video, with the given name. 
-    Also removes the temp folder after creating the video
+    """Looks for frames in given folder,
+    writes them into a video file, with the given name. 
+    Also removes the folder after creating the video.
     """
     if os.path.isdir(folder_path):
         create_video_from_images(folder_path, 'jpg', video_name, FPS)
 
-        # delete temp folder
+        # delete folder
         shutil.rmtree(folder_path)
 
 
@@ -105,13 +107,13 @@ def run_lk(video_name):
     _prep_temp_folder(LK_TEMP_FOLDER)
 
     # create some random colors one for each corner
-    color = np.random.randint(0, 255, (FEATURE_PARAMS['maxCorners'], 3))
+    colors = np.random.randint(0, 255, (FEATURE_PARAMS['maxCorners'], 3))
 
     # create video capture; capture first frame
     vid_cap = cv.VideoCapture(video_name)
     ret, frame_1 = vid_cap.read()
-    cur = convert_to_grayscale(frame_1)
-    cur_points = cv.goodFeaturesToTrack(cur, mask=None, **FEATURE_PARAMS)
+    cur_frame = convert_to_grayscale(frame_1)
+    cur_points = cv.goodFeaturesToTrack(cur_frame, mask=None, **FEATURE_PARAMS)
 
     # create a mask for drawing tracks
     mask = np.zeros_like(frame_1)
@@ -122,24 +124,21 @@ def run_lk(video_name):
         ret, frame_2 = vid_cap.read()
         if not ret:
             break
-        nxt = convert_to_grayscale(frame_2)
+        nxt_frame = convert_to_grayscale(frame_2)
 
 
         # compute optical flow between current and next frame
-        nxt_points, stdev, err = cv.calcOpticalFlowPyrLK(cur, nxt, cur_points, None, **LK_PARAMS)
+        cur_points, nxt_points, stdev, err = compute_optical_flow_LK(cur_frame, nxt_frame, cur_points, LK_PARAMS)
 
-        # select good points
-        good_nxt = nxt_points[stdev==1]
+        # select good points, with standard deviation 1. use numpy index trick
         good_cur = cur_points[stdev==1]
+        good_nxt = nxt_points[stdev==1]
 
-        # add tracks for all points
-        for i, (new, old) in enumerate(zip(good_nxt, good_cur)):
-            x1, y1 = new.ravel()
-            x0, y0 = old.ravel()
-            mask = cv.line(mask, (x1, y1), (x0, y0), GREEN_CV, 1)
-            frame = cv.circle(frame_2, (x1, y1), 3, GREEN_CV, -1)
-        img = cv.add(frame, mask)
-        img = draw_sparse_optical_flow_arrows(img, cur_points, nxt_points, thickness=1, arrow_scale=10.0, color=RED_CV)
+        # create img with added tracks for all point pairs on next frame
+        img, mask = draw_tracks(frame_2, good_cur, good_nxt, colors, mask, track_thickness=2)
+
+        # add optical flow arrows 
+        img = draw_sparse_optical_flow_arrows(img, good_cur, good_nxt, thickness=2, arrow_scale=10.0, color=RED_CV)
 
         # save image
         _frame_num += 1
@@ -147,13 +146,14 @@ def run_lk(video_name):
         img_path = os.path.join(LK_TEMP_FOLDER, img_name)
         cv.imwrite(img_path, img)
 
-        cur = nxt.copy()
-        cur_points = good_nxt.reshape(-1, 1, 2)
+        # ready for next iteration
+        cur_frame = nxt_frame.copy()
+        cur_points = good_nxt.reshape(-1, 1, 2) # -1 indicates to infer that dim size
 
-        # every n seconds (30 frames since we run at FPS =30), get good points
+        # every n seconds (n*FPS frames), get good points
         num_seconds = 1
-        if _frame_num%(num_seconds*FPS) == 0:
-            cur_points = cv.goodFeaturesToTrack(cur, mask=None, **FEATURE_PARAMS)
+        if _frame_num % (num_seconds*FPS) == 0:
+            cur_points = cv.goodFeaturesToTrack(cur_frame, mask=None, **FEATURE_PARAMS)
             # for every point in good point if its not there in cur points, add , update color too
             
 
@@ -165,9 +165,9 @@ if __name__ == "__main__":
     # while the game runs press key 's' to toggle screenshot mechanism on/off
     # initially screen saving is set to False
 
-    RUN_SIM     = 0
+    RUN_SIM     = 1
     RUN_FARN    = 0
-    RUN_LK      = 1
+    RUN_LK      = 0
 
     if RUN_SIM:
         # start game simulation
