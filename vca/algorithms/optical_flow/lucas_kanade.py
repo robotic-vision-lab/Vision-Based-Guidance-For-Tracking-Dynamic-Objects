0d60b713b6835edc324abbff0af771ff245f7965
Pritam Karmokar <pritam.karmokar@mavs.uta.edu>
2020-10-13 16:03:53 -0500

    Update tester section to make it more comprehensive

--- vca/algorithms/optical_flow/lucas_kanade.py
+++ vca/algorithms/optical_flow/lucas_kanade.py
@@ -57,12 +57,15 @@ if __name__ == "__main__":
     """ test Lucas Kanade implementation """
 
     # create dummy test images 
-    height = 20
-    width = 20
+    height = 400
+    width = 400
     data_path = generate_synth_data( img_size=(height, width), 
                                      path=os.path.join(vca_path,'../datasets'), 
                                      num_images=4, 
-                                     folder_name='synth_data' )
+                                     folder_name='synth_data_2',
+                                     radius=100,
+                                     num_dots=16,
+                                     increment=10 )
 
     # gather the path params needed in a dictionary
     synth_path_params = {'path':data_path, 'image_type':'jpg'}
@@ -78,7 +81,7 @@ if __name__ == "__main__":
                     'venus':venus_path_params }
     
     # list out the image path
-    img_paths = get_image_paths(**path_params['car'])
+    img_paths = get_image_paths(**path_params['synth'])
 
     # read and preprocess
     img_1 = normalize_to_255_range(preprocess_image(cv.imread(img_paths[0])))
@@ -110,24 +113,37 @@ if __name__ == "__main__":
     pts_1 = cv.goodFeaturesToTrack(img_gray_1, mask=None, **feature_params)
     _, pts_2, st, err = compute_optical_flow_LK( img_1=img_gray_1, 
                                                  img_2=img_gray_2, 
-                                                 pts_1=p1, 
+                                                 pts_1=pts_1, 
                                                  lk_params=lk_params )
 
     # select good points
     good_1 = pts_1[st==1]
     good_2 = pts_2[st==1]
 
+    img1=cv.cvtColor(img_1, cv.COLOR_GRAY2BGR)
+    img2=cv.cvtColor(img_2, cv.COLOR_GRAY2BGR)
     # draw tracks
     for i, (new, old) in enumerate(zip(good_2, good_1)):
         a,b = new.ravel()
         c,d = old.ravel()
-        mask = cv.line(mask, (a,b), (c,d), color[i].tolist(), 2) 
-        frame = cv.circle(cv.cvtColor(img_2, cv.COLOR_GRAY2BGR), (a,b), 5, color[i].tolist(), -1)
-    img = cv.add(frame, mask)   # lines and circles drawn on img_2
-    cv.imshow('before', img)
-    # draw arrows
-    img = draw_optical_flow_arrows(img, good_1, good_2)
+        # print(f'[{c}, {d}]\t->\t[{a}, {b}]')
+        # draw line (track between new and old points)
+        mask = cv.line(mask, (a,b), (c,d), (102,255,102), 2) 
+        # draw circle at new point
+        img1 = cv.circle(img1, (a,b), 5, (0,255,0), 1)
+        img2 = cv.circle(img2, (a,b), 5, (0,255,0), 1)
     
-    cv.imshow('frame', img)
+    img1[mask>0]=0
+    img2[mask>0]=0
+    img1 = cv.add(img1, mask)   # lines and circles drawn on img_2
+    img2 = cv.add(img2, mask)   # lines and circles drawn on img_2
+    cv.imshow('I1 with track and point', img1)
+    cv.imshow('I2 with track and point', img2)
+    # draw arrows
+    ar_scale = 1.0
+    img1 = draw_sparse_optical_flow_arrows(img1, good_1, good_2, thickness=1, arrow_scale=ar_scale, color=(51,51,255))
+    img2 = draw_sparse_optical_flow_arrows(img2, good_1, good_2, thickness=1, arrow_scale=ar_scale, color=(51,51,255))
+    cv.imshow(f'I1 with arrow (arrow_scale={ar_scale})', img1)
+    cv.imshow(f'I2 with arrow (arrow_scale={ar_scale})', img2)
     k = cv.waitKey(0)
 
