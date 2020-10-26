e273ad12e90fa116b671c9c85bd92a7dc4b4c67c
pritam <pritam.karmokar@gmail.com>
2020-10-26 14:35:27 -0500

    Add salt pepper

--- vca/utils/img_utils.py
+++ vca/utils/img_utils.py
@@ -116,6 +116,17 @@ def preprocess_image(img, blur=True):
     return img
 
 
+def add_salt_pepper(img, SNR):
+    img_ = img.copy()
+    img_ = img_.transpose(2,1,0)
+    c, h, w = img_.shape
+    mask = np.random.choice((0, 1, 2), size=(1, h, w), p=[SNR, (1 - SNR) / 2., (1 - SNR) / 2.])
+    mask = np.repeat(mask, c, axis=0) # Copy by channel to have the same shape as img
+    img_[mask == 1] = 255 # salt noise
+    img_[mask == 2] = 0 # 
+    img_ = img_.transpose(2,1,0)
+    return img_
+
 def scale_image(img, scale_factor=1.0):
     """ takes in img and scale_factor and returns scaled image with aspect ratio preserved """
     # compute dsize
