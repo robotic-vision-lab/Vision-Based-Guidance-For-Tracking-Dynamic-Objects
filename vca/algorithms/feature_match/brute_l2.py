11a958fa12f5d259b0e0c788130f87f6ec5a3aba
pritam <pritam.karmokar@gmail.com>
2020-12-28 22:36:48 -0600

    Add method to compute match distance between two descriptors

--- vca/algorithms/feature_match/brute_l2.py
+++ vca/algorithms/feature_match/brute_l2.py
@@ -5,20 +5,30 @@ class BruteL2:
     def __init__(self):
         self.matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_BRUTEFORCE)
 
-    def compute_matches(self, descriptors_1, descriptors_2):
-        return self.matcher.match(descriptors_1, descriptors_2)
-
-    @staticmethod
-    def draw_matches(self, img_1, kp_1, img_2, kp_2, matches):
-        img = np.empty((max(img_1.shape[0], img_2.shape[0]), img_1.shape[1]+img_2.shape[1], 3), 
-                        dtype=np.uint8)
+    def compute_matches(self, descriptors_1, descriptors_2, threshold=0.15):
+        matchers = self.matcher.match(descriptors_1, descriptors_2)
 
         distances = [m.distance for m in matches]
         mxd = max(distances)
         mnd = min(distances)
-        matches_ = [m for m in matches if m.distance/mxd < mnd/mxd + 0.15]
+        matches_ = [m for m in matches if m.distance/mxd < mnd/mxd + threshold]
 
-        img = cv.drawMatches(img_1, kp_1, img_2, kp_2, matches_, img)
-        cv.imshow('Good Matches', img)
+        return matches_
+
+    @staticmethod
+    def draw_matches(img_1, kp_1, img_2, kp_2, matches):
+        img = np.empty((max(img_1.shape[0], img_2.shape[0]), img_1.shape[1]+img_2.shape[1], 3), 
+                        dtype=np.uint8)
+
+        img = cv.drawMatches(img_1, kp_1, img_2, kp_2, matches, img)
+        cv.imshow('Matches', img)
         cv.waitKey(0)
         cv.destroyAllWindows()
+
+    def compute_descriptor_match_distance(self, des_1, des_2):
+        des_1 = np.array([des_1])
+        des_2 = np.array([des_2])
+
+        m = self.matcher.match(des_1, des_2)
+
+        return m[0].distance
