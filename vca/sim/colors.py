dcf5a14bc517ef70b7753e327086941685229df3
pritam <pritam.karmokar@gmail.com>
2021-01-06 19:51:36 -0600

    Add colors module

---
+++ vca/sim/colors.py
@@ -0,0 +1,64 @@
+# define some colors 
+OCEAN_GREEN_RGB = [99, 185, 149]
+CHINESE_RED_RGB = [174, 56, 29]
+DAVYS_GREY_RGB = [87, 90, 89]
+CULTURED_RGB = [250, 250, 250]
+ORANGE_PEEL_RGB = [255, 159, 28]
+PERSIAN_BLUE_RGB = [27, 44, 193]
+CORNFLOWER_BLUE_RGB = [118, 146, 255]
+CAROLINA_BLUE_RGB = [61, 165, 217]
+TIFFANY_BLUE_RGB = [7, 190, 184]
+HOT_PINK_RGB = [243, 116, 174]
+MARIGOLD_RGB = [241, 162, 8]
+GREEN_MUNSELL_RGB = [6, 167, 125]
+CARROT_ORANGE_RGB = [234, 144, 16]
+EMERALD_RGB = [35, 206, 107]
+SAFETY_YELLOW_RGB = [241, 211, 2]
+RUBY_RGB = [216, 17, 89]
+TOMATO_RGB = [254, 101, 79]
+VERMILION_RGB = [205, 70, 49]
+HONEY_YELLOW_RGB = [255, 173, 5]
+RADICAL_RED_RGB = [255, 51, 102]
+TURQUOISE_GREEN_RGB = [192, 229, 200]
+
+# define some colors to be used with pygame 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+DARK_GRAY = (31, 31, 31)
+DARK_GRAY_2 = (40, 40, 40)
+LIGHT_GRAY = (128, 128, 128)
+LIGHT_GRAY_2 = (153, 153, 153)
+LIGHT_GRAY_3 = (250, 250, 250)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (102, 102, 255)
+YELLOW = (255, 255, 0)
+
+# define colors to be used with opencv (BGR)
+RED_CV = (102, 102, 255)
+GREEN_CV = (102, 255, 102)
+BLUE_CV = (255, 102, 102)
+YELLOW_CV = (0, 255, 255)
+ORANGE_CV = (0, 165, 255)
+
+OCEAN_GREEN_BGR = list(reversed([99, 185, 149]))
+CHINESE_RED_BGR = list(reversed([174, 56, 29]))
+DAVYS_GREY_BGR = list(reversed([87, 90, 89]))
+CULTURED_BGR = list(reversed([250, 250, 250]))
+ORANGE_PEEL_BGR = list(reversed([255, 159, 28]))
+PERSIAN_BLUE_BGR = list(reversed([27, 44, 193]))
+CORNFLOWER_BLUE_BGR = list(reversed([118, 146, 255]))
+CAROLINA_BLUE_BGR = list(reversed([61, 165, 217]))
+TIFFANY_BLUE_BGR = list(reversed([7, 190, 184]))
+HOT_PINK_BGR = list(reversed([243, 116, 174]))
+MARIGOLD_BGR = list(reversed([241, 162, 8]))
+GREEN_MUNSELL_BGR = list(reversed([6, 167, 125]))
+CARROT_ORANGE_BGR = list(reversed([234, 144, 16]))
+EMERALD_BGR = list(reversed([35, 206, 107]))
+SAFETY_YELLOW_BGR = list(reversed([241, 211, 2]))
+RUBY_BGR = list(reversed([216, 17, 89]))
+TOMATO_BGR = list(reversed([254, 101, 79]))
+VERMILION_BGR = list(reversed([205, 70, 49]))
+HONEY_YELLOW_BGR = list(reversed([255, 173, 5]))
+RADICAL_RED_BGR = list(reversed([255, 51, 102]))
+TURQUOISE_GREEN_BGR = list(reversed([192, 229, 200]))
