bcf19224e2bbabbc0756833ee218cbc9f594688c
pritam <pritam.karmokar@gmail.com>
2021-01-16 19:40:25 -0600

    Add template matcher object to delegate template matching

--- vca/algorithms/template_match/corr_coeff_norm.py
+++ vca/algorithms/template_match/corr_coeff_norm.py
@@ -10,5 +10,35 @@ class CorrelationCoeffNormed:
 
         return result
 
+class TemplateMatcher:
+    def __init__(self, template, template_matcher):
+        self.template = template
+        self.matcher = template_matcher
+        self.match_result = None
+
+    def match_in_image(self, img):
+        self.match_result = self.matcher.compute_match(img, self.template)
+        return self.match_result
+
+    def find_template_in_image(self, img):
+        self.match_in_image(img)
+        self.min_val, self.max_val, self.min_loc, self.max_loc = cv.minMaxLoc(self.match_result)
+        w = self.template.shape[1]
+        h = self.template.shape[0]
+        self.top_left = self.max_loc
+        self.bottom_right = (self.top_left[0]+w, self.top_left[1]+h)
+
+        return self.top_left, self.bottom_right
+
+    def find_template_in_image_bb(self, img, bb):
+        x,y,w,h = bb
+        img_patch = img[y:y+h, x:x+w]
+        tf, br =  self.find_template_in_image(img_patch)
+        
+        self.top_left = (tf[0]+x, tf[1]+y)
+        self.bottom_right = (br[0]+x, br[1]+y)
+        
+        return self.top_left, self.bottom_right
+
     
     
