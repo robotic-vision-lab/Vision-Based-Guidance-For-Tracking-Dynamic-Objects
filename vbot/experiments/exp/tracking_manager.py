3f7355117bffaf3f0a7f3fc8f7012cbfc351ad1c
pritam <pritam.karmokar@gmail.com>
2021-07-09 09:56:21 -0500

    Add target setter and point collector from each target for ellipse enclosing

--- vbot/experiments/exp/tracking_manager.py
+++ vbot/experiments/exp/tracking_manager.py
@@ -1,5 +1,17 @@
 class TrackingManager:
     """[summary]
     """
-    def __init__(self, e_manager):
-        pass
+    def __init__(self, exp_manager):
+        self.exp_manager = exp_manager
+        self.targets = None
+
+    def set_targets(self, targets):
+        self.targets = targets
+
+    def get_points_to_be_enclosed(self):
+        points = []
+        for target in self.targets:
+            for point in target.get_bb_4_points():
+                points.append(point)
+
+        return points
