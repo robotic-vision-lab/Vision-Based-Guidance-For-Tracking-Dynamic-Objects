951f30de332126d0305bb0839b3476b8d38def05
pritam <pritam.karmokar@gmail.com>
2021-07-10 21:42:22 -0500

    Add comments for todo

--- vbot/experiments/exp/exp_manager.py
+++ vbot/experiments/exp/exp_manager.py
@@ -324,6 +324,31 @@ class ExperimentManager:
                             )
                             cv.imshow('Tracking in progress', self.multi_tracker.frame_color_edited);cv.waitKey(1)
                             
+
+                            '''
+                            At this point we have ellipse parameters
+                            next steps
+                            1. compute the following
+                                ellipse_center_x, 
+                                ellipse_center_y, 
+                                ellipse_focal_length, 
+                                ellipse_focal_point_1_x,
+                                ellipse_focal_point_1_y,
+                                ellipse_focal_point_2_x,
+                                ellipse_focal_point_2_y,
+                            2. filter focal_points
+                            3. convert to r1, θ1, r2, θ2. Compute focal_point speed and heading, acceleration and angle of acceleration
+                            4. Using all that compute a_lat and a_long
+                            5. feed accleration to simulator
+                            '''
+
+
+
+
+
+
+
+
                                                        
                             # let controller process kinematics
                             # ax, ay = self.controller.generate_acceleration(self.targets[0].kinematics)
