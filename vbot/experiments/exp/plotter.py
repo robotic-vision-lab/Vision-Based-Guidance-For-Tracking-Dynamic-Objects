6bbd3181cbafea0322671d72520669adbb7dfce7
pritam <pritam.karmokar@gmail.com>
2021-09-02 11:46:21 -0500

    Add new module to handle plotting systematically

---
+++ vbot/experiments/exp/plotter.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from .settings import *
+
+class LOS1DataPlotter:
+    def __init__(self, t, r1_t, r1_m, r1_e, r2_t, r2_m, r2_e, th1_t, th1_m, th1_e, th2_t, th2_m, th2_e):
+        self.t = t
+        self.r1_t = r1_t
+        self.r1_m = r1_m
+        self.r1_e = r1_e
+        self.r2_t = r2_t
+        self.r2_m = r2_m
+        self.r2_e = r2_e
+        self.th1_t = th1_t
+        self.th1_m = th1_m
+        self.th1_e = th1_e
+        self.th2_t = th2_t
+        self.th2_m = th2_m
+        self.th2_e = th2_e
+
+        self.window_title = 'LOS Kinematics - I'
+
+        self.set_params()
+
+    def set_params(self):
+
+        self.r1_t_params = dict(color='dimgray',     ls='-', lw=2, label=r'$r$')
+        self.r1_m_params = dict(color='forestgreen', ls='-', lw=1.5, label=r'$r_m$')
+        self.r1_e_params = dict(color='darkorange',     ls='-', lw=1.5, label=r'$\hat{r}$')
+        
+
+
+    def plot(self):
+        fig, axs = plt.subplots(2,1, sharex=True, gridspec_kw={'hspace': 0.25})
+        fig.suptitle(r'$\mathbf{Line\ of\ Sight\ Kinematics\ -\ I}$', fontsize=TITLE_FONT_SIZE)
+        fig.canvas.manager.set_window_title(self.window_title)
+
+        axs[0].plot(t, )
+
+
+    
+
+    
+
+
+
+    
