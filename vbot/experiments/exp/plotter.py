26decd0b7f492a2bcaf9fd750b1d4fd6d5432cc5
pritam <pritam.karmokar@gmail.com>
2021-09-02 16:49:27 -0500

    Add focal point to axes

--- vbot/experiments/exp/plotter.py
+++ vbot/experiments/exp/plotter.py
@@ -6,7 +6,32 @@ from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 from .settings import *
 
 class LOS1DataPlotter:
-    def __init__(self, save_path, t, r1_t, r1_m, r1_e, r2_t, r2_m, r2_e, th1_t, th1_m, th1_e, th2_t, th2_m, th2_e):
+    def __init__(self, 
+                 save_path, 
+                 t, 
+                 r1_t, 
+                 r1_m, 
+                 r1_e,
+                 r2_t,
+                 r2_m,
+                 r2_e,
+                 r3_t,
+                 r3_m,
+                 r3_e,
+                 f1_r,
+                 f2_r,
+                 th1_t,
+                 th1_m,
+                 th1_e,
+                 th2_t,
+                 th2_m,
+                 th2_e,
+                 th3_t,
+                 th3_m,
+                 th3_e,
+                 f1_th,
+                 f2_th):
+
         self.save_path = save_path
         self.t = t
         self.r1_t = r1_t
@@ -15,12 +40,23 @@ class LOS1DataPlotter:
         self.r2_t = r2_t
         self.r2_m = r2_m
         self.r2_e = r2_e
+        self.r3_t = r3_t
+        self.r3_m = r3_m
+        self.r3_e = r3_e
+        self.f1_r = f1_r
+        self.f2_r = f2_r
         self.th1_t = th1_t
         self.th1_m = th1_m
         self.th1_e = th1_e
         self.th2_t = th2_t
         self.th2_m = th2_m
         self.th2_e = th2_e
+        self.th3_t = th3_t
+        self.th3_m = th3_m
+        self.th3_e = th3_e
+        self.f1_th = f1_th
+        self.f2_th = f2_th
+
 
         self.window_title = 'LOS Kinematics - I'
         self.fig = None
@@ -31,24 +67,42 @@ class LOS1DataPlotter:
 
     def set_params(self):
         # r1 params
-        self.r1_t_params = dict(color='darkorchid',  alpha=0.7,  ls='-', lw=2,   label=r'$r_{1}$')
+        self.r1_t_params = dict(color='darkorchid',  alpha=0.7,  ls=':', lw=2,   label=r'$r_{1}$')
         self.r1_m_params = dict(color='forestgreen', alpha=0.85, ls='-', lw=1.5, label=r'$r_{1m}$')
         self.r1_e_params = dict(color='darkorange',  alpha=0.85, ls='-', lw=1.5, label=r'$\hat{r}_{1}$')
         
         # r2 params
-        self.r2_t_params = dict(color='darkorchid',  alpha=0.7,  ls='-', lw=2,   label=r'$r_{2}$')
+        self.r2_t_params = dict(color='darkorchid',  alpha=0.7,  ls=':', lw=2,   label=r'$r_{2}$')
         self.r2_m_params = dict(color='forestgreen', alpha=0.85, ls='-', lw=1.5, label=r'$r_{2m}$')
         self.r2_e_params = dict(color='darkorange',  alpha=0.85, ls='-', lw=1.5, label=r'$\hat{r}_{2}$')
+        
+        # r3 params
+        self.r3_t_params = dict(color='darkorchid',  alpha=0.7,  ls=':', lw=2,   label=r'$r_{3}$')
+        self.r3_m_params = dict(color='forestgreen', alpha=0.85, ls='-', lw=1.5, label=r'$r_{3m}$')
+        self.r3_e_params = dict(color='darkorange',  alpha=0.85, ls='-', lw=1.5, label=r'$\hat{r}_{3}$')
+        
+        # fp1r params
+        self.f1_r_params = dict(color='mediumvioletred',  alpha=0.85, ls='-', lw=1.5, label=r'$\hat{r}_{fp1}$')
+        self.f2_r_params = dict(color='mediumvioletred',  alpha=0.85, ls='-', lw=1.5, label=r'$\hat{r}_{fp2}$')
 
         # th1 params
-        self.th1_t_params = dict(color='darkorchid',  alpha=0.7,  ls='-', lw=2,   label=r'$\theta_{1}$')
+        self.th1_t_params = dict(color='darkorchid',  alpha=0.7,  ls=':', lw=2,   label=r'$\theta_{1}$')
         self.th1_m_params = dict(color='forestgreen', alpha=0.85, ls='-', lw=1.5, label=r'$\theta_{1m}$')
         self.th1_e_params = dict(color='darkorange',  alpha=0.85, ls='-', lw=1.5, label=r'$\hat{\theta}_{1}$')
         
         # th2 params
-        self.th2_t_params = dict(color='darkorchid',  alpha=0.7,  ls='-', lw=2,   label=r'$\theta_{2}$')
+        self.th2_t_params = dict(color='darkorchid',  alpha=0.7,  ls=':', lw=2,   label=r'$\theta_{2}$')
         self.th2_m_params = dict(color='forestgreen', alpha=0.85, ls='-', lw=1.5, label=r'$\theta_{2m}$')
         self.th2_e_params = dict(color='darkorange',  alpha=0.85, ls='-', lw=1.5, label=r'$\hat{\theta}_{2}$')
+        
+        # th3 params
+        self.th3_t_params = dict(color='darkorchid',  alpha=0.7,  ls=':', lw=2,   label=r'$\theta_{3}$')
+        self.th3_m_params = dict(color='forestgreen', alpha=0.85, ls='-', lw=1.5, label=r'$\theta_{3m}$')
+        self.th3_e_params = dict(color='darkorange',  alpha=0.85, ls='-', lw=1.5, label=r'$\hat{\theta}_{3}$')
+        
+        # fp1r params
+        self.f1_th_params = dict(color='mediumvioletred',  alpha=0.85, ls='-', lw=1.5, label=r'$\hat{\theta}_{fp1}$')
+        self.f2_th_params = dict(color='mediumvioletred',  alpha=0.85, ls='-', lw=1.5, label=r'$\hat{\theta}_{fp2}$')
 
         # rcParams
         params = {'xtick.direction'     : 'in',
@@ -88,6 +142,14 @@ class LOS1DataPlotter:
         self.axs[0].plot(self.t, self.r2_m, **self.r2_m_params)
         self.axs[0].plot(self.t, self.r2_e, **self.r2_e_params)
 
+        self.axs[0].plot(self.t, self.r3_t, **self.r3_t_params)
+        self.axs[0].plot(self.t, self.r3_m, **self.r3_m_params)
+        self.axs[0].plot(self.t, self.r3_e, **self.r3_e_params)
+
+        self.axs[0].plot(self.t, self.f1_r, **self.f1_r_params)
+        self.axs[0].plot(self.t, self.f2_r, **self.f2_r_params)
+        
+
         # th1, th2
         self.axs[1].plot(self.t, self.th1_t, **self.th1_t_params)
         self.axs[1].plot(self.t, self.th1_m, **self.th1_m_params)
@@ -96,6 +158,13 @@ class LOS1DataPlotter:
         self.axs[1].plot(self.t, self.th2_t, **self.th2_t_params)
         self.axs[1].plot(self.t, self.th2_m, **self.th2_m_params)
         self.axs[1].plot(self.t, self.th2_e, **self.th2_e_params)
+
+        self.axs[1].plot(self.t, self.th3_t, **self.th3_t_params)
+        self.axs[1].plot(self.t, self.th3_m, **self.th3_m_params)
+        self.axs[1].plot(self.t, self.th3_e, **self.th3_e_params)
+
+        self.axs[1].plot(self.t, self.f1_th, **self.f1_th_params)
+        self.axs[1].plot(self.t, self.f2_th, **self.f2_th_params)
 
         # set axes decorations
         self.add_axes_decor()
