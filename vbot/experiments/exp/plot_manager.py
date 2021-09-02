963f9a2f0e901edce02e4dc8112423dd86514609
pritam <pritam.karmokar@gmail.com>
2021-09-02 17:28:00 -0500

    Change plot style

--- vbot/experiments/exp/plot_manager.py
+++ vbot/experiments/exp/plot_manager.py
@@ -11,7 +11,7 @@ class PlotManager:
         self.skip_count = 0
         self.skip_step = 50
         plt.ion()   # <- sets rcParams['interactive'] = True, makes matplotlib redraw after every plotting command
-        plt.style.use(['ggplot','fast'])
+        plt.style.use(['seaborn-paper','fast'])
         # self.focal_points_plotter = FocalPointsPlotter(self, exp_manager, 'Focal points')
         self.uas_focal_points_plotter = UASFocalPointsPlotter(self, exp_manager, 'UAS and Focal points')
 
