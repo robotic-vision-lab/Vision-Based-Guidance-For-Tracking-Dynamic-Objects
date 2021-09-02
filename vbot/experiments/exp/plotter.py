85004241da53ab4de75aec822e2c0c190093d15e
pritam <pritam.karmokar@gmail.com>
2021-09-02 12:50:31 -0500

    Update LOS 1 plotter

--- vbot/experiments/exp/plotter.py
+++ vbot/experiments/exp/plotter.py
@@ -1,9 +1,13 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+
 from .settings import *
 
 class LOS1DataPlotter:
-    def __init__(self, t, r1_t, r1_m, r1_e, r2_t, r2_m, r2_e, th1_t, th1_m, th1_e, th2_t, th2_m, th2_e):
+    def __init__(self, save_path, t, r1_t, r1_m, r1_e, r2_t, r2_m, r2_e, th1_t, th1_m, th1_e, th2_t, th2_m, th2_e):
+        self.save_path = save_path
         self.t = t
         self.r1_t = r1_t
         self.r1_m = r1_m
@@ -45,6 +49,13 @@ class LOS1DataPlotter:
         self.th2_t_params = dict(color='darkorchid',     alpha=0.85, ls='-', lw=2,   label=r'$r$')
         self.th2_m_params = dict(color='forestgreen', alpha=0.85, ls='-', lw=1.5, label=r'$r_m$')
         self.th2_e_params = dict(color='darkorange',  alpha=0.85, ls='-', lw=1.5, label=r'$\hat{r}$')
+
+        # rcParams
+        mpl.rcParams['xtick.direction'] = 'in'
+        mpl.rcParams['ytick.direction'] = 'in'
+        mpl.rcParams['text.usetex'] = True
+        mpl.rcParams['pdf.compression'] = 0
+        
         
 
 
@@ -74,9 +85,26 @@ class LOS1DataPlotter:
         # set axes decorations
         self.add_axes_decor()
 
+        # save and show figure
+        self.fig.savefig(f'{self.save_path}/1_los1.pdf')
+        self.fig.show()
+
     def add_axes_decor(self):
+        self.axs[0].set_title(r'$\mathbf{r}$', fontsize=SUB_TITLE_FONT_SIZE)
+        self.axs[0].legend(loc='upper right')
+        self.axs[0].set(ylabel=r'$r\ (m)$')
+        self.axs[0].xaxis.set_minor_locator(AutoMinorLocator())
+        self.axs[0].yaxis.set_minor_locator(AutoMinorLocator())
+        self.axs[0].grid(True, which='minor', alpha=0.1)
+        self.axs[0].grid(True, which='major', alpha=0.3)
         
-        
+        self.axs[1].set_title(r'$\mathbf{\theta}$', fontsize=SUB_TITLE_FONT_SIZE)
+        self.axs[1].legend(loc='upper right')
+        self.axs[1].set(xlabel=r'$time\ (s)$', ylabel=r'$\theta\ (^{\circ})$')
+        self.axs[1].xaxis.set_minor_locator(AutoMinorLocator())
+        self.axs[1].yaxis.set_minor_locator(AutoMinorLocator())
+        self.axs[1].grid(True, which='minor', alpha=0.1)
+        self.axs[1].grid(True, which='major', alpha=0.3)
 
 
     
