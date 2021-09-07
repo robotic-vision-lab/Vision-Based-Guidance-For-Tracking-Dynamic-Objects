596c585a0260060fb28e8d132a4d8e923213ecd9
pritam <pritam.karmokar@gmail.com>
2021-09-07 10:17:19 -0500

    Edit all car  trajectories to squircle

--- vbot/experiments/exp_mot_sf/simulator.py
+++ vbot/experiments/exp_mot_sf/simulator.py
@@ -120,9 +120,9 @@ class Simulator:
             self.blocks.append(Block(self))
 
         # spawn car
-        self.car = Car(self, *CAR_INITIAL_POSITION, *CAR_INITIAL_VELOCITY, *CAR_ACCELERATION, loaded_image_rect=self.car_img_rect, img=CAR_IMG)
-        self.car_2 = Car(self, *CAR_INITIAL_POSITION_2, *CAR_INITIAL_VELOCITY_2, *CAR_ACCELERATION, loaded_image_rect=self.car_img_rect_2, img=CAR_IMG_2)
-        self.car_3 = Car(self, *CAR_INITIAL_POSITION_3, *CAR_INITIAL_VELOCITY_3, *CAR_ACCELERATION, loaded_image_rect=self.car_img_rect_3,img=CAR_IMG_3,traj=LANE_CHANGE_TRAJECTORY)
+        self.car = Car(self, *CAR_INITIAL_POSITION, *CAR_INITIAL_VELOCITY, *CAR_ACCELERATION, loaded_image_rect=self.car_img_rect, img=CAR_IMG,traj=SQUIRCLE_TRAJECTORY)
+        self.car_2 = Car(self, *CAR_INITIAL_POSITION_2, *CAR_INITIAL_VELOCITY_2, *CAR_ACCELERATION, loaded_image_rect=self.car_img_rect_2, img=CAR_IMG_2,traj=SQUIRCLE_TRAJECTORY)
+        self.car_3 = Car(self, *CAR_INITIAL_POSITION_3, *CAR_INITIAL_VELOCITY_3, *CAR_ACCELERATION, loaded_image_rect=self.car_img_rect_3,img=CAR_IMG_3,traj=SQUIRCLE_TRAJECTORY)
 
         #spawn bar
         self.bars = []
