2ed1be18e2fcf92de1f8338a3d4365e5996ed955
pritam <pritam.karmokar@gmail.com>
2021-09-18 17:25:55 -0500

    Wisely discretize/pixelize only when you must

--- vbot/experiments/exp_mot_lc_1/tracking_manager.py
+++ vbot/experiments/exp_mot_lc_1/tracking_manager.py
@@ -8,7 +8,7 @@ from pygame.locals import *
 from .ellipse import Ellipse2D
 from .bounding_area_EKF import BoundingAreaEKF
 from .settings import *
-
+import matplotlib.pyplot as plt
 
 
 class TrackingManager:
@@ -24,6 +24,7 @@ class TrackingManager:
         self.ellipse_params_est = None
 
         self.stored_data = None
+        plt.ion()
 
         # # for debugging 
         # self.temp = False
@@ -107,13 +108,166 @@ class TrackingManager:
 
 
     def display(self):
+        # # collect all params
+        # ellipse_params = self.get_ellipse_params(IMAGE_REF_FRAME)
+        # ellipse_semi_axes = tuple(map(int, ellipse_params[:2]))
+        # ellipse_center = tuple(map(int, ellipse_params[2]))
+        # ellipse_rotation_angle = ellipse_params[3]  # radians
+        
+        
+        # ellipse_focal_point_1 = tuple(map(int, ellipse_params[4]))
+        # ellipse_focal_point_2 = tuple(map(int, ellipse_params[5]))
+
+        # # note: focal point estimations must be called before calling display
+        # ellipse_focal_point_1_est = (self.ellipse_params_est[0],
+        #                             self.ellipse_params_est[3])
+        # ellipse_focal_point_2_est = (self.ellipse_params_est[6],
+        #                             self.ellipse_params_est[9])
+
+        # ellipse_focal_point_1_est = self.convert(ellipse_focal_point_1_est)
+        # ellipse_focal_point_2_est = self.convert(ellipse_focal_point_2_est)
+        # ellipse_focal_point_1_est = tuple(map(int, ellipse_focal_point_1_est))
+        # ellipse_focal_point_2_est = tuple(map(int, ellipse_focal_point_2_est))
+
+        # # compute axis aligned bounding box
+        # # use parametric equation for ellipse, parameterized by t
+        # #   x = h + a cos(t) cos(φ) - b sin(t) sin(φ) 
+        # #   y = k + b sin(t) cos(φ) + a cos(t) sin(φ)
+        # # where (h, k) is ellipse center, a is semimajor axis, b is semiminor axis, φ is angle of rotation of ellipse
+        # # we can find extremum of x and y wrt t to find xmax, xmin, ymax, ymin as follows
+
+        # h = (ellipse_focal_point_1_est[0] + ellipse_focal_point_2_est[0])//2
+        # k = (ellipse_focal_point_1_est[1] + ellipse_focal_point_2_est[1])//2
+        
+        # # extremum of x w.r.t t => dx/dt=0
+        # t_x = atan((-ellipse_semi_axes[1] / ellipse_semi_axes[0]) * tan(ellipse_rotation_angle))
+
+        # # extremum of y w.r.t t => dy/dt=0
+        # t_y = atan((ellipse_semi_axes[1] / ellipse_semi_axes[0]) / (tan(ellipse_rotation_angle)+1e-16))
+
+        # # compute xmax, xmin
+        # x1 = int(h + ellipse_semi_axes[0] * cos(ellipse_rotation_angle) * cos(t_x%tau)    - ellipse_semi_axes[1] * sin(ellipse_rotation_angle) * sin(t_x%tau))
+        # x2 = int(h + ellipse_semi_axes[0] * cos(ellipse_rotation_angle) * cos((t_x+pi)%tau) - ellipse_semi_axes[1] * sin(ellipse_rotation_angle) * sin((t_x+pi)%tau))
+        # x_max, x_min = (x1, x2) if x1 > x2 else (x2, x1)
+
+        # # compute ymax, ymin
+        # y1 = int(k + ellipse_semi_axes[0] * sin(ellipse_rotation_angle) * cos(t_y%tau)    + ellipse_semi_axes[1] * cos(ellipse_rotation_angle) * sin(t_y%tau))
+        # y2 = int(k + ellipse_semi_axes[0] * sin(ellipse_rotation_angle) * cos((t_y+pi)%tau) + ellipse_semi_axes[1] * cos(ellipse_rotation_angle) * sin((t_y+pi)%tau))
+        # y_max, y_min = (y1, y2) if y1 > y2 else (y2, y1)
+
+        # # form corners for the axis aligned bounding box of ellipse
+        # self.p1 = (x_min, y_min)
+        # self.p2 = (x_max, y_max)
+
+        # # compute midpoint of control area and C boundary coords
+        # self.pmid = ((x_min+x_max)//2, (y_min+y_max)//2)
+        # C_DES = self.exp_manager.controller.C_DES
+        # c1 = tuple(map(int,(SCREEN_CENTER[0]-C_DES, SCREEN_CENTER[1]-C_DES)))
+        # c2 = tuple(map(int,(SCREEN_CENTER[0]+C_DES, SCREEN_CENTER[1]+C_DES)))
+        # colr = (204, 204, 204) if self.exp_manager.controller.e_c_prev >= 0 else TOMATO_BGR
+        
+        # # draw over color edited frame and show it
+        # ellipse_img = np.zeros_like(self.exp_manager.multi_tracker.frame_color_edited, np.uint8)
+
+        
+        # # draw axis aligned bounding box
+        # ellipse_img = cv.rectangle(ellipse_img,
+        #                            self.p1,
+        #                            self.p2,
+        #                            (217, 232, 212),
+        #                            2,
+        #                            cv.LINE_4)
+        # # draw axis aligned bounding box min max points
+        # ellipse_img = cv.circle(ellipse_img,
+        #                         self.p1,
+        #                         radius=3,
+        #                         color=(255, 51, 51),
+        #                         thickness=cv.FILLED,
+        #                         lineType=cv.LINE_8)
+        # ellipse_img = cv.circle(ellipse_img,
+        #                         self.p2,
+        #                         radius=3,
+        #                         color=(51, 51, 255),
+        #                         thickness=cv.FILLED,
+        #                         lineType=cv.LINE_8)
+
+        # # draw filled ellipse
+        # ellipse_img = cv.ellipse(img=ellipse_img,
+        #                          center=ellipse_center,
+        #                          axes=ellipse_semi_axes,
+        #                          angle=-degrees(ellipse_rotation_angle),
+        #                          startAngle=0,
+        #                          endAngle=360,
+        #                          color=ELLIPSE_COLOR,
+        #                          thickness=cv.FILLED,
+        #                          lineType=cv.LINE_8)
+        # # draw C boundary
+        # ellipse_img = cv.rectangle(ellipse_img,
+        #                            c1,
+        #                            c2,
+        #                            colr,
+        #                            2,
+        #                            cv.LINE_4)
+        # ellipse_img = cv.circle(ellipse_img,
+        #                         self.pmid,
+        #                         radius=4,
+        #                         color=colr,
+        #                         thickness=cv.FILLED,
+        #                         lineType=cv.LINE_AA)    
+        # # draw measured focal points
+        # ellipse_img = cv.circle(ellipse_img,
+        #                         ellipse_focal_point_1,
+        #                         radius=ELLIPSE_MEAS_FP_RADIUS,
+        #                         color=ELLIPSE_MEAS_FP_COLOR,
+        #                         thickness=cv.FILLED,
+        #                         lineType=cv.LINE_AA)
+        # ellipse_img = cv.circle(ellipse_img,
+        #                         ellipse_focal_point_2,
+        #                         radius=ELLIPSE_MEAS_FP_RADIUS,
+        #                         color=ELLIPSE_MEAS_FP_COLOR,
+        #                         thickness=cv.FILLED,
+        #                         lineType=cv.LINE_AA)
+        
+        # # draw estimated focal points
+        # ellipse_img = cv.circle(ellipse_img,
+        #                         ellipse_focal_point_1_est,
+        #                         radius=ELLIPSE_ESTD_FP_RADIUS,
+        #                         color=ELLIPSE_ESTD_FP_COLOR,
+        #                         thickness=cv.FILLED,
+        #                         lineType=cv.LINE_AA)
+        # ellipse_img = cv.circle(ellipse_img,
+        #                         ellipse_focal_point_2_est,
+        #                         radius=ELLIPSE_ESTD_FP_RADIUS,
+        #                         color=ELLIPSE_ESTD_FP_COLOR,
+        #                         thickness=cv.FILLED,
+        #                         lineType=cv.LINE_AA)
+
+        # # blend this with color edited frame
+        # blended_img = self.exp_manager.multi_tracker.frame_color_edited.copy()
+        # mask = ellipse_img.astype(bool)
+        # blended_img[mask] = cv.addWeighted(self.exp_manager.multi_tracker.frame_color_edited, 
+        #                                    1 - ELLIPSE_OPACITY,
+        #                                    ellipse_img,
+        #                                    ELLIPSE_OPACITY,
+        #                                    0)[mask]
+
+        # # save it for screensaver
+        # self.exp_manager.multi_tracker.cur_img = blended_img
+
+        # # show blended image
+        # cv.imshow(self.exp_manager.multi_tracker.win_name, blended_img);cv.waitKey(1)
+
+
+
         # collect all params
         ellipse_params = self.get_ellipse_params(IMAGE_REF_FRAME)
-        ellipse_semi_axes = tuple(map(int, ellipse_params[:2]))
-        ellipse_center = tuple(map(int, ellipse_params[2]))
+        ellipse_semi_axes = ellipse_params[:2]
+        ellipse_center = ellipse_params[2]
         ellipse_rotation_angle = ellipse_params[3]  # radians
-        ellipse_focal_point_1 = tuple(map(int, ellipse_params[4]))
-        ellipse_focal_point_2 = tuple(map(int, ellipse_params[5]))
+        
+        
+        ellipse_focal_point_1 = ellipse_params[4]
+        ellipse_focal_point_2 = ellipse_params[5]
 
         # note: focal point estimations must be called before calling display
         ellipse_focal_point_1_est = (self.ellipse_params_est[0],
@@ -123,8 +277,8 @@ class TrackingManager:
 
         ellipse_focal_point_1_est = self.convert(ellipse_focal_point_1_est)
         ellipse_focal_point_2_est = self.convert(ellipse_focal_point_2_est)
-        ellipse_focal_point_1_est = tuple(map(int, ellipse_focal_point_1_est))
-        ellipse_focal_point_2_est = tuple(map(int, ellipse_focal_point_2_est))
+        ellipse_focal_point_1_est = ellipse_focal_point_1_est
+        ellipse_focal_point_2_est = ellipse_focal_point_2_est
 
         # compute axis aligned bounding box
         # use parametric equation for ellipse, parameterized by t
@@ -143,13 +297,13 @@ class TrackingManager:
         t_y = atan((ellipse_semi_axes[1] / ellipse_semi_axes[0]) / (tan(ellipse_rotation_angle)+1e-16))
 
         # compute xmax, xmin
-        x1 = int(h + ellipse_semi_axes[0] * cos(ellipse_rotation_angle) * cos(t_x%tau)    - ellipse_semi_axes[1] * sin(ellipse_rotation_angle) * sin(t_x%tau))
-        x2 = int(h + ellipse_semi_axes[0] * cos(ellipse_rotation_angle) * cos((t_x+pi)%tau) - ellipse_semi_axes[1] * sin(ellipse_rotation_angle) * sin((t_x+pi)%tau))
+        x1 = h + ellipse_semi_axes[0] * cos(ellipse_rotation_angle) * cos(t_x%tau)    - ellipse_semi_axes[1] * sin(ellipse_rotation_angle) * sin(t_x%tau)
+        x2 = h + ellipse_semi_axes[0] * cos(ellipse_rotation_angle) * cos((t_x+pi)%tau) - ellipse_semi_axes[1] * sin(ellipse_rotation_angle) * sin((t_x+pi)%tau)
         x_max, x_min = (x1, x2) if x1 > x2 else (x2, x1)
 
         # compute ymax, ymin
-        y1 = int(k + ellipse_semi_axes[0] * sin(ellipse_rotation_angle) * cos(t_y%tau)    + ellipse_semi_axes[1] * cos(ellipse_rotation_angle) * sin(t_y%tau))
-        y2 = int(k + ellipse_semi_axes[0] * sin(ellipse_rotation_angle) * cos((t_y+pi)%tau) + ellipse_semi_axes[1] * cos(ellipse_rotation_angle) * sin((t_y+pi)%tau))
+        y1 = k + ellipse_semi_axes[0] * sin(ellipse_rotation_angle) * cos(t_y%tau)    + ellipse_semi_axes[1] * cos(ellipse_rotation_angle) * sin(t_y%tau)
+        y2 = k + ellipse_semi_axes[0] * sin(ellipse_rotation_angle) * cos((t_y+pi)%tau) + ellipse_semi_axes[1] * cos(ellipse_rotation_angle) * sin((t_y+pi)%tau)
         y_max, y_min = (y1, y2) if y1 > y2 else (y2, y1)
 
         # form corners for the axis aligned bounding box of ellipse
@@ -159,8 +313,8 @@ class TrackingManager:
         # compute midpoint of control area and C boundary coords
         self.pmid = ((x_min+x_max)//2, (y_min+y_max)//2)
         C_DES = self.exp_manager.controller.C_DES
-        c1 = tuple(map(int,(SCREEN_CENTER[0]-C_DES, SCREEN_CENTER[1]-C_DES)))
-        c2 = tuple(map(int,(SCREEN_CENTER[0]+C_DES, SCREEN_CENTER[1]+C_DES)))
+        c1 = (SCREEN_CENTER[0]-C_DES, SCREEN_CENTER[1]-C_DES)
+        c2 = (SCREEN_CENTER[0]+C_DES, SCREEN_CENTER[1]+C_DES)
         colr = (204, 204, 204) if self.exp_manager.controller.e_c_prev >= 0 else TOMATO_BGR
         
         # draw over color edited frame and show it
@@ -169,20 +323,20 @@ class TrackingManager:
         
         # draw axis aligned bounding box
         ellipse_img = cv.rectangle(ellipse_img,
-                                   self.p1,
-                                   self.p2,
+                                   tuple(map(int,self.p1)),
+                                   tuple(map(int,self.p2)),
                                    (217, 232, 212),
                                    2,
                                    cv.LINE_4)
         # draw axis aligned bounding box min max points
         ellipse_img = cv.circle(ellipse_img,
-                                self.p1,
+                                tuple(map(int,self.p1)),
                                 radius=3,
                                 color=(255, 51, 51),
                                 thickness=cv.FILLED,
                                 lineType=cv.LINE_8)
         ellipse_img = cv.circle(ellipse_img,
-                                self.p2,
+                                tuple(map(int,self.p2)),
                                 radius=3,
                                 color=(51, 51, 255),
                                 thickness=cv.FILLED,
@@ -190,7 +344,7 @@ class TrackingManager:
 
         # draw filled ellipse
         ellipse_img = cv.ellipse(img=ellipse_img,
-                                 center=ellipse_center,
+                                 center=tuple(map(int,ellipse_center)),
                                  axes=ellipse_semi_axes,
                                  angle=-degrees(ellipse_rotation_angle),
                                  startAngle=0,
@@ -200,26 +354,26 @@ class TrackingManager:
                                  lineType=cv.LINE_8)
         # draw C boundary
         ellipse_img = cv.rectangle(ellipse_img,
-                                   c1,
-                                   c2,
+                                   tuple(map(int,c1)),
+                                   tuple(map(int,c2)),
                                    colr,
                                    2,
                                    cv.LINE_4)
         ellipse_img = cv.circle(ellipse_img,
-                                self.pmid,
+                                tuple(map(int,self.pmid)),
                                 radius=4,
                                 color=colr,
                                 thickness=cv.FILLED,
                                 lineType=cv.LINE_AA)    
         # draw measured focal points
         ellipse_img = cv.circle(ellipse_img,
-                                ellipse_focal_point_1,
+                                tuple(map(int,ellipse_focal_point_1)),
                                 radius=ELLIPSE_MEAS_FP_RADIUS,
                                 color=ELLIPSE_MEAS_FP_COLOR,
                                 thickness=cv.FILLED,
                                 lineType=cv.LINE_AA)
         ellipse_img = cv.circle(ellipse_img,
-                                ellipse_focal_point_2,
+                                tuple(map(int,ellipse_focal_point_2)),
                                 radius=ELLIPSE_MEAS_FP_RADIUS,
                                 color=ELLIPSE_MEAS_FP_COLOR,
                                 thickness=cv.FILLED,
@@ -227,13 +381,13 @@ class TrackingManager:
         
         # draw estimated focal points
         ellipse_img = cv.circle(ellipse_img,
-                                ellipse_focal_point_1_est,
+                                tuple(map(int,ellipse_focal_point_1_est)),
                                 radius=ELLIPSE_ESTD_FP_RADIUS,
                                 color=ELLIPSE_ESTD_FP_COLOR,
                                 thickness=cv.FILLED,
                                 lineType=cv.LINE_AA)
         ellipse_img = cv.circle(ellipse_img,
-                                ellipse_focal_point_2_est,
+                                tuple(map(int,ellipse_focal_point_2_est)),
                                 radius=ELLIPSE_ESTD_FP_RADIUS,
                                 color=ELLIPSE_ESTD_FP_COLOR,
                                 thickness=cv.FILLED,
